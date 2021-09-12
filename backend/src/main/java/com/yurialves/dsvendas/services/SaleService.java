a230f367e2ca169a3936f53598a0b235add067e6
Yuri Alves <yuri.o.alves2@outlook.com>
2021-09-12 03:10:06 -0300

    Group by search

--- backend/src/main/java/com/yurialves/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/yurialves/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.yurialves.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yurialves.dsvendas.dto.SaleDTO;
+import com.yurialves.dsvendas.dto.SaleSuccessDTO;
+import com.yurialves.dsvendas.dto.SaleSumDTO;
 import com.yurialves.dsvendas.entities.Sale;
 import com.yurialves.dsvendas.repositories.SaleRepository;
 import com.yurialves.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
